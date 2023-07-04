3b2ebf9ff90e4e8c3cbcefafa9a878b44f5ad96e
huangyq123 <925102915@qq.com>
2023-07-04 12:28:30 +0800

    第二次提交，测试新分支push文件

---
+++ testgitone/src/main/java/com/yqhuang/git/testgitone/test1/testPush.java
@@ -0,0 +1,13 @@
+package com.yqhuang.git.testgitone.test1;
+
+/**
+ * @ClassName testPush
+ * @Description TODO
+ * @Author huangyq
+ * @Date 2023/7/4 12:24
+ * @Version
+ **/
+public class testPush {
+    private int age;
+
+}
